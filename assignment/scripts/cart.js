ed632dda6e6e8acc8805460451b7903ad405f91f
Myles Voigt <myles.voigt@gmail.com>
2022-10-16 16:20:37 -0500

    main goals done

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,38 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+
+function addItem(item){
+    basket.push(item)
+    if (basket.includes(item)){
+        return true;
+    }
+}
+
+//testing addItem function 
+let result = false;
+console.log(result = addItem('juice'));
+
+//adding items to basket to list off in listItems
+addItem('crackers');
+addItem('cheese');
+
+function listItems(){
+    for (let item of basket){
+        console.log(item);
+    }
+}
+
+//testing listItems function
+listItems();
+
+//function to empty the basket array
+function empty(){
+    basket = [];
+}
+
+//testing 'empty' function
+empty();
+console.log(basket);
